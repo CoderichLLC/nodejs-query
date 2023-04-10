e97db49b8239d1bbf3b348c259dd9b6e4571936f
Richard Livolsi <richard.livolsi@goziohealth.com>
2023-04-10 14:08:33 -0400

    Normalizing to Mongo syntax; normalizing data keys

--- src/Quin.js
+++ src/Quin.js
@@ -1,14 +1,16 @@
-const Flat = require('flat');
-
-// const counterOps = { eq: 'ne', ne: 'eq', lt: 'gte', lte: 'gt', gt: 'lte', gte: 'lt', in: 'nin', nin: 'in' };
+const Util = require('@coderich/util');
 
 module.exports = class Quin {
-  #query = { fields: ['*'] };
+  #query = Object.defineProperties({}, {
+    id: { writable: true },
+    keyMap: { writable: true, value: {} },
+  });
 
-  constructor(target) {
-    this.#query.from = target;
-    this.join = () => this;
-    this.sort = () => this;
+  constructor(query = {}) {
+    Object.assign(this.#query, query);
+
+    // this.join = () => this;
+    // this.sort = () => this;
 
     // Terminal commands
     // this.first = () => this.#query;
@@ -19,17 +21,18 @@ module.exports = class Quin {
   id(id) {
     this.#propCheck('id', 'where', 'sort', 'skip', 'limit', 'before', 'after', 'first', 'last');
     this.#query.id = id;
-    this.#query.where = { id };
+    this.#query.where = this.#normalize({ id });
     return this;
   }
 
-  where(...args) {
+  where(clause) {
     this.#propCheck('where', 'id');
-    this.#query.where = Quin.where(false, ...args);
+    this.#query.where = this.#normalize(clause);
     return this;
   }
 
   select(...args) {
+    this.#propCheck('fields');
     this.#query.fields = args.flat();
     return this;
   }
@@ -77,42 +80,62 @@ module.exports = class Quin {
   }
 
   one() {
-    return Object.assign(this.#query, { crud: 'readOne' });
+    return this.resolve(Object.assign(this.#query, { op: 'findOne' }));
   }
 
   many() {
-    return Object.assign(this.#query, { crud: 'readMany' });
+    return this.resolve(Object.assign(this.#query, { op: 'findMany' }));
   }
 
   save() {
-    if (this.#query.id) return Object.assign(this.#query, { crud: 'updateOne' });
-    if (this.#query.where) return Object.assign(this.#query, { crud: 'updateMany' });
-    return Object.assign(this.#query, { crud: 'createMany' });
+    if (this.#query.id) return this.resolve(Object.assign(this.#query, { op: 'updateOne' }));
+    if (this.#query.where) return this.resolve(Object.assign(this.#query, { op: 'updateMany' }));
+    return this.resolve(Object.assign(this.#query, { op: 'createMany' }));
   }
 
   delete() {
-    if (this.#query.id) return Object.assign(this.#query, { crud: 'deleteOne' });
-    if (this.#query.where) return Object.assign(this.#query, { crud: 'deleteMany' });
+    if (this.#query.id) return this.resolve(Object.assign(this.#query, { op: 'deleteOne' }));
+    if (this.#query.where) return this.resolve(Object.assign(this.#query, { op: 'deleteMany' }));
     throw new Error('Delete requires id() or where()');
+  }
+
+  clone(query = {}) {
+    query.keyMap = query.keyMap || this.#query.keyMap;
+    return new Quin({ ...this.#query, ...query });
+  }
+
+  resolve() {
+    return this.#query;
+  }
+
+  #normalize(data) {
+    return Util.unflatten(Object.entries(Util.flatten(data)).reduce((prev, [key, value]) => {
+      // Rename key
+      const $key = Object.entries(this.#query.keyMap).reduce((p, [k, v]) => {
+        const regex = new RegExp(`((?:^|\\.))${k}\\b`, 'g');
+        return p.replace(regex, `$1${v}`);
+      }, key);
+
+      // Assign it back
+      return Object.assign(prev, { [$key]: value });
+    }, {}));
+
+    // return args.flat().map((arg) => {
+    //   return Object.entries(Flat.flatten(arg, { safe: true })).reduce((arr, [key, value]) => {
+    //     let op;
+    //     [key, op = 'eq'] = key.split('.$');
+    //     // if (key.startsWith('$not')) return arr.concat(...Quin.where(true, value));
+    //     if (key.startsWith('$or')) return arr.concat([Quin.where(negate, value)]);
+    //     if (Array.isArray(value)) op = 'in';
+    //     // if (negate) op = counterOps[op];
+    //     return arr.concat({ key, [op]: value });
+    //   }, []);
+    // });
   }
 
   #propCheck(prop, ...checks) {
     if (['skip', 'limit'].includes(prop) && this.isCursorPaging) throw new Error(`Cannot use "${prop}" while using Cursor-Style Pagination`);
     if (['first', 'last', 'before', 'after'].includes(prop) && this.isClassicPaging) throw new Error(`Cannot use "${prop}" while using Classic-Style Pagination`);
     checks.forEach((check) => { if (this.#query[check]) throw new Error(`Cannot use "${prop}" while using "${check}"`); });
-  }
-
-  static where(negate = false, ...args) {
-    return args.flat().map((arg) => {
-      return Object.entries(Flat.flatten(arg, { safe: true })).reduce((arr, [key, value]) => {
-        let op;
-        [key, op = 'eq'] = key.split('.$');
-        // if (key.startsWith('$not')) return arr.concat(...Quin.where(true, value));
-        if (key.startsWith('$or')) return arr.concat([Quin.where(negate, value)]);
-        if (Array.isArray(value)) op = 'in';
-        // if (negate) op = counterOps[op];
-        return arr.concat({ key, [op]: value });
-      }, []);
-    });
   }
 };
--- test/Quin.test.js
+++ test/Quin.test.js
@@ -1,89 +1,30 @@
 const Quin = require('../src/Quin');
 
 describe('Quin', () => {
-  // test('where (negation)', () => {
-  //   expect(Quin.where(true, {
-  //     $not: [
-  //       {
-  //         a: 'rich',
-  //         c: ['a', 'b'],
-  //       },
-  //       {
-  //         d: { $gt: '21' },
-  //         e: { $in: ['a', 'b'] },
-  //       },
-  //     ],
-  //   })).toEqual([
-  //     [
-  //       { key: 'a', ne: 'rich' },
-  //       { key: 'c', nin: ['a', 'b'] },
-  //       { key: 'd', lte: '21' },
-  //       { key: 'e', nin: ['a', 'b'] },
-  //     ],
-  //   ]);
-  // });
-
-  test('where', () => {
-    expect(Quin.where(false, {
-      a: 'rich',
-      b: /rich/i,
-      c: ['a', 'b'],
-      d: { $gt: '21' },
-      e: { $in: ['a', 'b'] },
-      f: { a: { b: 'c' } },
-      'a.b.d': { $lt: 4, $lte: 4 },
-      'a.b.e': { $gte: 1, $lte: 100 },
-      $or: [
-        {
-          'a.b.d.$lt': 5,
-          'a.b.d.$lte': 5,
-          'a.b.e': { $gte: 100, $lte: 1000 },
-        },
-        {
-          'a.b.d.$lte': 1,
-          $or: [{
-            'a.b.d.$gt': 100,
-          }],
-        },
-      ],
-    }, {
-      a: 'anne',
-    })).toEqual([
-      [
-        { key: 'a', eq: 'rich' },
-        { key: 'b', eq: /rich/i },
-        { key: 'c', in: ['a', 'b'] },
-        { key: 'd', gt: '21' },
-        { key: 'e', in: ['a', 'b'] },
-        { key: 'f.a.b', eq: 'c' },
-        { key: 'a.b.d', lt: 4 },
-        { key: 'a.b.d', lte: 4 },
-        { key: 'a.b.e', gte: 1 },
-        { key: 'a.b.e', lte: 100 },
-        [
-          [
-            { key: 'a.b.d', lt: 5 },
-            { key: 'a.b.d', lte: 5 },
-            { key: 'a.b.e', gte: 100 },
-            { key: 'a.b.e', lte: 1000 },
-          ],
-          [
-            { key: 'a.b.d', lte: 1 },
-            [[{ key: 'a.b.d', gt: 100 }]],
-          ],
-        ],
-      ],
-      [
-        { key: 'a', eq: 'anne' },
-      ],
-    ]);
+  test('Basic queries', () => {
+    expect(new Quin({ model: 'table' }).id(1).one()).toEqual({ op: 'findOne', model: 'table', where: { id: 1 } });
+    expect(new Quin({ model: 'table' }).id(1).select('a', 'b', 'c').one()).toEqual({ op: 'findOne', model: 'table', fields: ['a', 'b', 'c'], where: { id: 1 } });
+    expect(new Quin({ model: 'table' }).id(1).select(['a', 'b', 'c']).one()).toEqual({ op: 'findOne', model: 'table', fields: ['a', 'b', 'c'], where: { id: 1 } });
+    expect(new Quin({ model: 'table' }).one()).toEqual({ op: 'findOne', model: 'table' });
+    expect(new Quin({ model: 'table' }).many()).toEqual({ op: 'findMany', model: 'table' });
   });
 
-  test('reads', () => {
-    expect(new Quin('table1').one()).toEqual({ crud: 'readOne', fields: ['*'], from: 'table1' });
-    expect(new Quin('table1').select('*').one()).toEqual({ crud: 'readOne', fields: ['*'], from: 'table1' });
-    expect(new Quin('table1').select(['a', 'b', 'c']).one()).toEqual({ crud: 'readOne', fields: ['a', 'b', 'c'], from: 'table1' });
-    expect(new Quin('table1').select('a', 'b', 'c').one()).toEqual({ crud: 'readOne', fields: ['a', 'b', 'c'], from: 'table1' });
-    expect(new Quin('table1').select('a', 'b', 'c').many()).toEqual({ crud: 'readMany', fields: ['a', 'b', 'c'], from: 'table1' });
+  test('Clone', () => {
+    const quin = new Quin({ model: 'table' });
+    expect(quin.clone().id(1).one()).toEqual({ op: 'findOne', model: 'table', where: { id: 1 } });
+    expect(quin.clone().id(1).select(['a', 'b', 'c']).one()).toEqual({ op: 'findOne', model: 'table', fields: ['a', 'b', 'c'], where: { id: 1 } });
+    expect(quin.clone().one()).toEqual({ op: 'findOne', model: 'table' });
+    expect(quin.clone().many()).toEqual({ op: 'findMany', model: 'table' });
+  });
+
+  test('Query manipulation', () => {
+    const quin = new Quin({ model: 'table', keyMap: { id: '_id' } });
+    expect(quin.clone().where({ id: 1 }).one()).toEqual({ op: 'findOne', model: 'table', where: { _id: 1 } });
+    expect(quin.clone().where({ network: { id: 1 } }).one()).toEqual({ op: 'findOne', model: 'table', where: { network: { _id: 1 } } });
+    expect(quin.clone().where({ 'network.id': 1 }).one()).toEqual({ op: 'findOne', model: 'table', where: { 'network._id': 1 } });
+    expect(quin.clone().where({ networkid: 1 }).one()).toEqual({ op: 'findOne', model: 'table', where: { networkid: 1 } });
+    expect(quin.clone().where({ 'my.array': ['a', 'b', 'c'] }).one()).toEqual({ op: 'findOne', model: 'table', where: { 'my.array': ['a', 'b', 'c'] } });
+    expect(quin.clone().where({ $or: [{ id: 1 }, { network: { id: 2 } }] }).one()).toEqual({ op: 'findOne', model: 'table', where: { $or: [{ _id: 1 }, { network: { _id: 2 } }] } });
+    expect(quin.clone().where({ $or: [{ id: 1 }, { 'network.id': 2 }] }).one()).toEqual({ op: 'findOne', model: 'table', where: { $or: [{ _id: 1 }, { 'network._id': 2 }] } });
   });
 });
